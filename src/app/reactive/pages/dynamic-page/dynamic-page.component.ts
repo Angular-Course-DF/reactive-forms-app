7f69e32f20f8678ea0c2aa48cf6e3b96e99a5435
Daniel Forero <danielforero93@gmail.com>
2025-05-20 18:57:33 -0500

    Refactorización validaciones form array

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,51 @@
+import { FormUtils } from './../../../utils/form-utils';
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
 })
-export class DynamicPageComponent {}
+export class DynamicPageComponent {
+  private fb = inject(FormBuilder);
+
+  formUtils = FormUtils;
+
+  //hacerlo tipo formGroup facilita para verificar las propiedades del formulario
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array(
+      [
+        ['Metal Gear Solid', [Validators.required, Validators.minLength(3)]],
+        ['Final Fantasy VII', [Validators.required, Validators.minLength(3)]],
+      ],
+      [Validators.minLength(4)]
+    ),
+  });
+
+  getFavoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  // isValidFieldInArray(formArray: FormArray, index: number) {
+  //   return (
+  //     formArray.controls[index].errors && formArray.controls[index].touched
+  //   );
+  // }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
+}
--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,16 +1,7 @@
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 
 export class FormUtils {
-  static isValidField(form: FormGroup, fieldName: string): boolean | null {
-    return (
-      !!form.controls[fieldName].errors && form.controls[fieldName].touched
-    );
-  }
-  static getFieldError(form: FormGroup, fieldName: string): string | null {
-    if (!form.controls[fieldName]) return null;
-
-    const errors = form.controls[fieldName].errors ?? {};
-
+  static getErrorMessage(errors: ValidationErrors) {
     for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
@@ -23,24 +14,30 @@ export class FormUtils {
     }
     return null;
   }
-  // static getFieldError(
-  //   form: FormGroup,
-  //   fieldName: string
-  // ): string | null {
-  //   if (!form.controls[fieldName]) return null;
 
-  //   const errors = form.controls[fieldName].errors ?? {};
+  static isValidField(form: FormGroup, fieldName: string): boolean | null {
+    return (
+      !!form.controls[fieldName].errors && form.controls[fieldName].touched
+    );
+  }
 
-  //   for (const key of Object.keys(errors)) {
-  //     switch (key) {
-  //       case 'required':
-  //         return 'Este campo es requerido';
-  //       case 'minlength':
-  //         return `Este campo debe tener al menos ${errors[key].requiredLength} caracteres`;
-  //       case 'min':
-  //         return `Este campo debe ser mayor a ${errors[key].min}`;
-  //     }
-  //   }
-  //   return null;
-  // }
+  static getFieldError(form: FormGroup, fieldName: string): string | null {
+    if (!form.controls[fieldName]) return null;
+
+    const errors = form.controls[fieldName].errors ?? {};
+    return this.getErrorMessage(errors);
+  }
+
+  static isValidFieldInArray(formArray: FormArray, index: number) {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+  }
+
+  static getFieldErrorInArray(formArray: FormArray, index: number) {
+    if (!formArray.controls[index]) return null;
+
+    const errors = formArray.controls[index].errors ?? {};
+    return this.getErrorMessage(errors);
+  }
 }
