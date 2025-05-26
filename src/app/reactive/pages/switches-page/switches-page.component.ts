bafbdedb8a6a68b96fe95f85b63e56e1a75b1605
Daniel Forero <danielforero93@gmail.com>
2025-05-26 11:05:12 -0500

    Switches, checks y radio buttons

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,27 @@
+import { FormUtils } from './../../../utils/form-utils';
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html',
 })
-export class SwitchesPageComponent {}
+export class SwitchesPageComponent {
+  private fb = inject(FormBuilder);
+
+  formUtils = FormUtils;
+  myForm = this.fb.group({
+    // gender: ['M', Validators.required],
+    gender: [, Validators.required],
+    wantsNotifications: [true],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  submit() {
+    this.myForm.markAllAsTouched();
+
+    console.log(this.myForm.value);
+  }
+}
