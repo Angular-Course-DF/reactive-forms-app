b29d0cd125132dd3b19df1347fd8b7fd72a94357
Daniel Forero <danielforero93@gmail.com>
2025-06-12 11:50:16 -0500

    Poblar select continentes

---
+++ src/app/country/services/country.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Country } from '../interfaces/country.interface';
+
+@Injectable({ providedIn: 'root' })
+export class CountryService {
+  private http = inject(HttpClient);
+  private readonly apiUrl = 'https://restcountries.com/v3.1';
+
+  private _regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  //por si se hace alguna modificación, se haría sobre ese getter y no en el array
+  get regions(): string[] {
+    return [...this._regions];
+  }
+
+  getCountriesByRegion(region: string): Observable<Country[]> {
+    if (!region) return of([]);
+    console.log(region);
+    const url = `${this.apiUrl}/all?fields=name,cca3,borders`;
+    return this.http.get<Country[]>(`${url}/region/${region}`);
+  }
+
+  getCountryByAlphaCode(alphaCode: string): Observable<Country> {
+    const url = `${this.apiUrl}/alpha/${alphaCode}?fields=name,cca3,borders`;
+    return this.http.get<Country>(`${url}`);
+  }
+
+  getCountryBordersByCodes(borders: string[]) {
+    //TODO: Implementar la lógica para obtener los países fronterizos
+  }
+}
